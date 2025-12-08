83a3c5f94c05cac5d0c7032b73c2b6a685778ab6
Trivenee Badgujar <161133137+TriveneeBadgujar@users.noreply.github.com>
2025-12-08 17:18:38 +0530

    Add files via upload

---
+++ Reverse_String.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char s[100];
+    strcpy(s, "Trivenee");
+
+    int i, len = strlen(s);
+    for(i = len - 1; i >= 0; i--)
+        printf("%c", s[i]);
+
+    return 0;
+}
